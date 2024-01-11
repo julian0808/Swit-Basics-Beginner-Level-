6356d961450114672101a1daadc9aa26eb6e5208
julian0808 <julianm08@gmail.com>
2024-01-11 17:52:13 -0500

    Initial Commit

---
+++ SwiftfulThinkingBasicsBootcamp.playground/Pages/BsicTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+// var allows you to change their value.
+
+// let gets equal to one thing and never changes.
+
+
+
+
+// String is "regular text"
+let myFirstItem: String = "Hello, world!"
+
+// Reference previously created objects
+let myTitle = myFirstItem
+
+//Boolean (aka bool) is true or false:
+let mySecondItem: Bool = true
+let myThirditem: Bool = false
+
+
+// Do ot do!
+// let myFourthIem: String = "true"
+
+// Swift is a type-safe language
+// let myFifthItem: Bool = "Hello, world!"
+let mySixthItem: String = "Hello, world!"
+let mySeventhItem =  "Hello, world!"
+
+// Date
+
+let myFirstDate: Date = Date()
+
+// Number - Can be Int, Double, CGFloat & more
+
+// Int is a whole Integer
+let myFirstNumber: Int = 1
+
+// Use Double for math
+let mySecondnumber: Double = 1.0
+
+// Use CG
+let myThirdnumber: CGFloat = 1.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SwiftfulThinkingBasicsBootcamp.playground/Pages/SwitfBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+
+// This is a single_single comment
+
+// multi line comment
+/*
+ This is a multi-line comment
+ that goes to
+ multiple lnes!
+ */
+
+// Naming Conversionts
+
+// Camel Case - CORRECT!
+// The first word is lowercased and the the first character in every following word is uppercase.
+
+let firstGreeting = "Hello, world!"
+let thisIsMyFirstGreeting = "Hello, world!"
+
+let thisismysecondgreeting = "Hello, world!"
+
+// Snake Case - wrong
+let this_is_what_snake_case_looks_like = "Hello, world!"
+
+// Pascal Case - Wrong
+let ThisIsWhatPascalCaseLookLike = "Hello, world!"
+
+// Camel Case - right
+let thisIsWhatCameCaseLooksLike = "Hello, world!"
+
+
+
